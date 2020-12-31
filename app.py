02f71844fba653f0914183359dba3c2990f2d010
Luis DeLeon <luismdeleon@gmail.com>
2020-12-31 14:35:17 -0800

    init commit with base app and test model

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask import jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return jsonify(hello="world!")
+
+
+if __name__ == '__main__':
+    app.run()
